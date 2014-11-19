3f3be887c299618d83eee823d604d0f32bc3a5dc
Tim Martin <timothy.m.martin@gmail.com>
2014-11-19 12:00:19 -0800

    Added function to format tweet timestam a litle more cleanly.

--- application.js
+++ application.js
@@ -13,9 +13,36 @@ function refreshTweets() {
   while(display >= 0) {
     var tweet = streams.home[index-count];
     var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' (' + formatDate(tweet.created_at) + ')');
     $tweet.appendTo($tweetDisplay);
     display--;
     count++;
   }
 };
+
+function formatDate(dateIn) {
+  var result = "";
+  dateIn += "";
+  var dateArr = dateIn.split(' ');
+  console.log(dateArr);
+  result = monAsNumber(dateArr[1])+"/"+dateArr[2]+"/"+dateArr[3]+" at "+dateArr[4];
+  return result;
+}
+
+function monAsNumber(mon) {
+  var months = {
+    "Jan": 1,
+    "Feb": 2,
+    "Mar": 3,
+    "Apr": 4,
+    "May": 5,
+    "Jun": 6,
+    "Jul": 7,
+    "Aug": 8,
+    "Sep": 9,
+    "Oct": 10,
+    "Nov": 11,
+    "Dec": 12
+  };
+  return months[mon];
+}
